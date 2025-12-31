c8f243eb9450b6bbb04bfe1ee963e95fe5ed9458
alikhaled09 <alikkamal1000@gmail.com>
2025-12-31 14:57:09 +0200

    test responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -499,12 +499,12 @@ const Navbar = () => {
 
           {/* RIGHT ACTIONS */}
           <div className="flex items-center gap-3">
-            {/* Mobile Gems (visible if user) - Moved to Dropdown Menu */}
-            {/* {user && (
+            {/* Mobile Gems (visible if user) */}
+            {user && (
               <div className="lg:hidden">
                 <GemsBadge balance={gemsBalance} onAddClick={() => setIsAddGemsModalOpen(true)} isLoading={isLoadingBalance} />
               </div>
-            )} */}
+            )}
 
             {/* Desktop Right */}
             <div className="hidden lg:flex lg:items-center lg:gap-4">
@@ -614,12 +614,7 @@ const Navbar = () => {
                   </div>
                 )}
 
-                {/* Mobile Gems (Added here instead of top bar) */}
-                {user && (
-                  <div className="px-4 pt-4 border-t border-gray-100 flex justify-center">
-                    <GemsBadge balance={gemsBalance} onAddClick={() => setIsAddGemsModalOpen(true)} isLoading={isLoadingBalance} />
-                  </div>
-                )}
+
               </div>
             </motion.div>
           )}
